0fdf2259047c002ea83805813725274778c1f4e0
imgoury02 <140312176+imgoury02@users.noreply.github.com>
2024-01-02 14:15:08 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// <![CDATA[
+    $(function() {
+
+        // Slider
+        $('#coin-slider').coinslider({width:960,height:360,opacity:1});
+      
+        // Radius Box
+        //$('.menu_nav ul li a, .content p.pages span, .content p.pages a').css({"border-radius":"6px", "-moz-border-radius":"6px", "-webkit-border-radius":"6px"});
+        //$('.content .mainbar img.fl, p.infopost, .searchform, .content .sidebar .gadget').css({"border-radius":"10px", "-moz-border-radius":"10px", "-webkit-border-radius":"10px"});
+        //$('.content p.pages span, .content p.pages a').css({"border-radius":"16px", "-moz-border-radius":"16px", "-webkit-border-radius":"16px"});
+        $('.menu_nav ul li a').css({"border-top-left-radius":"6px", "border-top-right-radius":"6px", "-moz-border-radius-topleft":"6px", "-moz-border-radius-topright":"6px", "-webkit-border-top-left-radius":"6px", "-webkit-border-top-right-radius":"6px"});
+      
+      });	
+      
+      
